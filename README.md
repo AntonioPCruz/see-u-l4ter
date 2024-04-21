d72a093a6ace13b1b789d4613546d58e421b85e7
Mateus Aleixo <mateus.aleixo@ubi.pt>
2024-04-21 18:04:56 +0100

    Add support for SQLite in sqlx dependency

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # main
+
 - [x] gerar chaves pseudo-aleatorias com uma cifra, dependentes do email, password, dia e hora (por exemplo SHA256) -> mostrar na pagina/cli
 - [ ] gerar chaves para uma determinada hora tambem dependentes do mesmo, para cifrar um ficheiro com essa chave -> gerar codigo HMAC-SHA256 ligado à chave
 - [ ] permitir tentar decifrar um criptograma com uma chave que o utilizador da. a essa chave adiciona-se o dia/hora atual -> o sistema verifica o codigo HMAC-SHA256 ligado a chave e avisa o utilizador se nao estiver certo
 
 # extra
+
 - [ ] escolher entre varios tipos de cifra
 - [ ] escolher HMAC entre 256 e 512
 - [ ] keypairs RSA para verificar se quem quer decifrar foi quem decifrou
@@ -13,3 +15,19 @@
 - [x] SSL/TLS ou LetsEncrypt para a ligacao do cli/site com o backend
 - [ ] base de dados da backend encriptada
 
+```bash
+make install
+
+curl -s -k \
+-w '\n' \
+-H 'Content-Type: application/json' \
+-d '{"name": "corno", "email" : "foo", "client_secret" : "bar"}' \
+-X POST https://localhost:3000/register
+
+curl -s -k \
+-w '\n' \
+-H 'Content-Type: application/json' \
+-d '{"email" : "foo", "client_secret" : "bar"}' \
+-X POST \
+https://localhost:3000/login
+```
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,16 +1,16 @@
 use axum::{
-    extract::Host,
-    extract::State,
+    extract::{Host, State},
     handler::HandlerWithoutStateExt,
     http::{StatusCode, Uri},
     response::Redirect,
     routing::{get, post},
-    BoxError, Json, Router,
+    BoxError, Extension, Json, Router,
 };
 use axum_server::tls_rustls::RustlsConfig;
 use dotenv::dotenv;
 use jsonwebtoken::{encode, Header as OtherHeader};
 use once_cell::sync::Lazy;
+use sqlx::Row;
 use sqlx::{sqlite::SqlitePool, Pool, Sqlite};
 use std::time::UNIX_EPOCH;
 use std::{net::SocketAddr, path::PathBuf};
@@ -68,7 +68,10 @@ async fn main() {
 
     println!("TLS certificates read!");
 
-    let sqlpool = SqlitePool::connect(std::env::var("DATABASE_URL")).await?;
+    let sqlpool: Pool<Sqlite> =
+        SqlitePool::connect(&std::env::var("DATABASE_URL").expect("oopsies"))
+            .await
+            .expect("oopsies");
 
     /* Routes:
        /api/now/encrypt   -> encrypt with a key relating to the current timestamp. Input: File to encrypt, Cipher used;       Outputs: Encrypted file and HMAC
@@ -86,9 +89,8 @@ async fn main() {
         .layer(axum::middleware::from_fn(jwt::refresh_middleware))
         .layer(axum::middleware::from_fn(keygen::keygen_middleware))
         .route("/login", post(login))
-        .with_state(sqlpool)
-        // .route("/register", get(register))
-    ;
+        .route("/register", post(register))
+        .layer(Extension(sqlpool));
 
     println!("TLS Server started!");
     // run https server
@@ -107,7 +109,7 @@ async fn protected(claims: Claims) -> Result<String, AuthError> {
 }
 
 async fn login(
-    pool: State<Pool<Sqlite>>,
+    Extension(pool): Extension<SqlitePool>,
     Json(payload): Json<AuthPayload>,
 ) -> Result<Json<AuthBody>, AuthError> {
     // Check if the user sent the credentials
@@ -118,9 +120,8 @@ async fn login(
     let user = sqlx::query("SELECT * FROM users WHERE ? = email AND ? = password")
         .bind(payload.email)
         .bind(payload.client_secret)
-        .fetch_one(pool)
-        .await
-        .expect("oopsies");
+        .fetch_one(&pool)
+        .await;
 
     let user = match user {
         Ok(u) => u,
@@ -132,7 +133,7 @@ async fn login(
         .expect("Time went backwards")
         .as_secs();
     let mut claims = Claims {
-        email: user.email,
+        email: user.get("email"),
         key: String::new(),
         key_timestamp: now + (60 * 5),
         exp: now as usize + (60 * 5), // 5 mins
@@ -149,7 +150,7 @@ async fn login(
 }
 
 async fn register(
-    pool: State<Pool<Sqlite>>,
+    Extension(pool): Extension<SqlitePool>,
     Json(payload): Json<RegisterPayload>,
 ) -> Result<Json<AuthBody>, AuthError> {
     // Check if the user sent the credentials
@@ -159,8 +160,8 @@ async fn register(
 
     let mut conn = pool.acquire().await.expect("oopsies");
     match sqlx::query("SELECT * FROM users WHERE ? = email")
-        .bind(payload.email)
-        .fetch_one(conn)
+        .bind(payload.email.clone())
+        .fetch_one(&pool)
         .await
     {
         Ok(u) => return Err(AuthError::DuplicateAccount),
@@ -170,10 +171,11 @@ async fn register(
     let user =
         sqlx::query("INSERT INTO users (name, email, password, verified) VALUES (?, ?, ?, 1)")
             .bind(payload.name)
-            .bind(payload.email)
+            .bind(payload.email.clone())
             .bind(payload.client_secret)
             .execute(&mut *conn)
-            .await?
+            .await
+            .expect("oopsies")
             .last_insert_rowid();
 
     let now = std::time::SystemTime::now()
