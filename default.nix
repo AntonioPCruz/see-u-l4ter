26f4a513a90b7837a81e2f5478ab28d7130c6c97
zazedd <leomendesantos@gmail.com>
2024-04-24 19:20:17 +0100

    updated nix build and shell, removed database

--- default.nix
+++ default.nix
@@ -2,8 +2,13 @@
 let manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
 pkgs.rustPlatform.buildRustPackage rec {
+  nativeBuildInputs = with pkgs; [ cmake pkg-config ncurses6.dev ];
+  OPENSSL_NO_VENDOR = 1;
+  PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+
   pname = manifest.name;
   version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
+  buildInputs = with pkgs; [ darwin.apple_sdk.frameworks.Foundation openssl_1_1 openssl_1_1.dev ];
 }
--- shell.nix
+++ shell.nix
@@ -2,7 +2,17 @@
 pkgs.mkShell {
   inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
   buildInputs = with pkgs; [
-    openssl
-    ncurses
+    openssl_1_1 
+    ncurses6
+    darwin.apple_sdk.frameworks.Foundation
   ];
+
+  shellHook = ''
+    export OPENSSL_DIR=${pkgs.openssl_1_1.dev};
+    export OPENSSL_INCLUDE_DIR=${pkgs.openssl_1_1.dev}/include;
+    export OPENSSL_LIB_DIR=${pkgs.openssl_1_1.out}/lib;
+    export OPENSSL_NO_VENDOR = 1;
+
+    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+  '';
 }
